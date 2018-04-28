ff7b5844d46798c0e44bac2bd6f6ce5bce1ec560
Siddharth Rajan <siddharthrajan10@gmail.com>
2018-04-28 17:09:12 -0400

    4/28 5:09pm

--- map.js
+++ map.js
@@ -1,5 +1,6 @@
 function initMap() {
-  var uluru = {lat: -25.363, lng: 131.044};
+	// 20.5937° N, 78.9629° E
+  var uluru = {lat: 40.7, lng: -74};
   var map = new google.maps.Map(document.getElementById('map'), {
     zoom: 4,
     center: uluru
