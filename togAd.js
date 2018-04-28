362ecf12e5a725485b494779ae2d6ea198181aa2
Siddharth Rajan <siddharthrajan10@gmail.com>
2018-04-28 14:41:15 -0400

    4/28 2:41pm

---
+++ togAd.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  $('#advert').click(function() {
+    $('#aditems').toggle();
+  })
+})
---
+++ togUser.js
@@ -0,0 +1,5 @@
+$(document).ready(() => {
+  $('#user').click(function() {
+    $('#useritems').toggle();
+  })
+})
