6666b670b866978ec2a6ead7d98a4fb8b2731077
Siddharth Rajan <siddharthrajan10@gmail.com>
2018-04-28 21:23:11 -0400

    4/28 9:23pm

--- map.js
+++ map.js
@@ -1,13 +1,35 @@
+var map, infoWindow;
 function initMap() {
-	// 20.5937° N, 78.9629° E
-  var ny = {lat: 40.7, lng: -74};
-  var map = new google.maps.Map(document.getElementById('map'), {
-    zoom: 4,
-    center: ny
-    });
-    var marker = new google.maps.Marker({
-      position: ny,
-      map: map
-    });
+	map = new google.maps.Map(document.getElementById('map'), {
+    center: {lat: -34.397, lng: 150.644},
+    zoom: 6
+ });
+ 
+infoWindow = new google.maps.InfoWindow;
+
+// Try HTML5 geolocation.
+if (navigator.geolocation) {
+	navigator.geolocation.getCurrentPosition(function(position) {
+    	var pos = {
+            lat: position.coords.latitude,
+            lng: position.coords.longitude
+        };
+
+        infoWindow.setPosition(pos);
+        infoWindow.setContent('Location found.');
+        infoWindow.open(map);
+        map.setCenter(pos);
+    }, function() {
+     handleLocationError(true, infoWindow, map.getCenter());
+     });
+} else {
+          // Browser doesn't support Geolocation
+handleLocationError(false, infoWindow, map.getCenter());
+}
 }
 
+function handleLocationError(browserHasGeolocation, infoWindow, pos) {
+    infoWindow.setPosition(pos);
+    infoWindow.setContent(browserHasGeolocation ? 'Error: The Geolocation service failed.' : 'Error: Your browser doesn\'t support geolocation.');
+    infoWindow.open(map);
+}
